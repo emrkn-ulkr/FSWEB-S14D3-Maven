c19d6df20648e3889c25be04686ea47382a07d22
r1semir <officialemirkan@gmail.com>
2025-11-13 16:37:59 +0300

    Main Input

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,44 @@
 package org.example;
 
 import org.example.arge.CarSkeleton;
+import org.example.company.Car;
+import org.example.company.Ford;
+import org.example.company.Holden;
+import org.example.company.Mitsubishi;
 
 public class Main {
     public static void main(String[] args) {
+        Car car = new Car(8, "Base car");
 
+        System.out.println(car.startEngine());
 
+        System.out.println(car.accelerate());
+
+        System.out.println(car.breake());
+
+        Car mitsubishi = new Mitsubishi(6, "Outlander VRX 4WD");
+
+        System.out.println(mitsubishi.startEngine());
+
+        System.out.println(mitsubishi.accelerate());
+
+        System.out.println(mitsubishi.breake());
+
+        Car ford = new Ford(6, "Ford Falcon");
+
+        System.out.println(ford.startEngine());
+
+        System.out.println(ford.accelerate());
+
+        System.out.println(ford.breake());
+
+        Car holden = new Holden(6, "Holden Commodore");
+
+        System.out.println(holden.startEngine());
+
+        System.out.println(holden.accelerate());
+
+        System.out.println(holden.breake());
 
     }
 }
